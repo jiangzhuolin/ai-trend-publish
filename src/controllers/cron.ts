0a2aece6747b895911604767be0f180f2c218f90
jiangzl <jiangzhuolin@outlook.com>
2026-05-07 12:09:28 +0800

    [chunk] 将定时执行的任务由原来的凌晨 03:00 改为 08:30

--- src/controllers/cron.ts
+++ src/controllers/cron.ts
@@ -46,9 +46,9 @@ export const startCronJobs = () => {
   barkNotifier.notify("定时任务启动", "定时任务启动");
   logger.info("初始化定时任务...");
 
-  // 每天凌晨3点执行
+  // 每天早上 08:30 执行
   cron.schedule(
-    "0 3 * * *",
+    "30 8 * * *",
     async () => {
       const dayOfWeek = new Date().getDay(); // 0是周日，1-6是周一到周六
       const adjustedDay = dayOfWeek === 0
